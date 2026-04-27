4aad13909fd126158cda9ef9bce6d5dd04fd1e1e
seohyeok <seohyeok.jang0@gmail.com>
2026-04-27 23:32:02 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,15 +9,22 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
+        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
     
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
     }
 }
