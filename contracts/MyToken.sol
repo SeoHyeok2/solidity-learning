6ab73021c4f102b3c92ef68b071291d107b07fcb
seohyeok <seohyeok.jang0@gmail.com>
2026-04-27 15:16:19 +0900

    mint function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -13,17 +13,11 @@ contract MyToken {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
+        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
-
-    // function totalSupply() external view returns (uint256) {
-    //     return totalSupply;
-    // }
-
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf[owner];
-    // }
-
-    // function name() external view returns (string memory) {
-    //     return name;
-    // }
+    
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+    }
 }
