6427cb02ca659c01e05773725e9e8e34791b9a18
seohyeok <seohyeok.jang0@gmail.com>
2026-05-05 23:09:49 +0900

    fix access manager to multi

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "./ManagedAccess.sol";
+import "./MultiManagedAccess.sol";
 
 interface IMyToken {
     function transfer(uint256 amount, address to) external;
@@ -11,7 +11,7 @@ interface IMyToken {
     function mint(uint256 amount, address owner) external;
 }
 
-contract TinyBank is ManagedAccess {
+contract TinyBank is MultiManagedAccess {
     event Staked(address from, uint256 amount);
     event Withdraw(uint256 amount, address to);
 
@@ -20,12 +20,15 @@ contract TinyBank is ManagedAccess {
     mapping(address => uint256) public lastClaimedBlock;
 
     uint256 defaultRewardPerBlock = 1 * 10 ** 18;
-    uint256 rewardPerBlock; 
+    uint256 public rewardPerBlock;
 
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
-    constructor(IMyToken _stackingToken) ManagedAccess(msg.sender, msg.sender) {
+    constructor(
+        IMyToken _stackingToken,
+        address[3] memory _managers
+        ) MultiManagedAccess(msg.sender, _managers) {
         stakingToken = _stackingToken;
         rewardPerBlock = defaultRewardPerBlock;
     }
@@ -40,7 +43,7 @@ contract TinyBank is ManagedAccess {
         _; // caller's code
     }
 
-    function setRewardPerBlock(uint256 _amount) external onlyManager {
+    function setRewardPerBlock(uint256 _amount) external onlyAllConfirmed {
         rewardPerBlock = _amount;
     }
 
