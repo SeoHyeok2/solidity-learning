53a2355572bd022ce7c2eb09e5869a4240de56c0
seohyeok <seohyeok.jang0@gmail.com>
2026-05-05 15:50:04 +0900

    add mint

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -33,6 +33,10 @@ contract MyToken {
         balanceOf[to] += amount;
         emit Transfer(from, to, amount);
     }
+
+    function mint(uint256 amount, address owner) external {
+        _mint(amount, owner);
+    }
     
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
