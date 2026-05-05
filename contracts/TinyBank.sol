aa4a8ea272140a81ecc2547c3671e88dbbd32307
seohyeok <seohyeok.jang0@gmail.com>
2026-05-05 18:59:50 +0900

    refactor updateReward by modifier

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -25,27 +25,26 @@ contract TinyBank {
         stakingToken = _stackingToken;
     }
 
-    function updateReward(address to) internal {
+    modifier updateReward(address to) {
         if (staked[to] > 0) {
             uint256 blocks = block.number - lastClaimedBlock[to];
             uint256 reward = (blocks * rewardPerBlock * staked[to]) / totalStaked;
             stakingToken.mint(reward, to);
         }
         lastClaimedBlock[to] = block.number;
+        _; // caller's code
     }
 
-    function stake(uint256 _amount) external {
+    function stake(uint256 _amount) external updateReward(msg.sender) {
         require(_amount >= 0, "cannot stake 0 amount");
-        updateReward(msg.sender);
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked(msg.sender, _amount);
     }
 
-    function withdraw(uint256 _amount) external {
+    function withdraw(uint256 _amount) external updateReward(msg.sender) {
         require(staked[msg.sender] >= _amount, "insufficient staked token");
-        updateReward(msg.sender);
         stakingToken.transfer(_amount, msg.sender);
         staked[msg.sender] -= _amount;
         totalStaked -= _amount;
